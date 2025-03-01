82ce60f9cc7a889a9384212b3621c90b9c49c8f3
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-01 23:13:05 +0900

    기본 세팅 및 Hello World build 확인

---
+++ CSharp_200/CSharp_200/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_200
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
