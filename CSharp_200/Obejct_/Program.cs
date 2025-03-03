b5c7ee58d59d9842a30eedb34d94980da344c8b8
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-04 02:18:05 +0900

    object

---
+++ CSharp_200/Obejct_/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Obejct_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object o = i;
+            i = i + 10;
+            int j = (int)o;
+
+
+            Console.WriteLine("The value-type value i = {0}" , i);
+            Console.WriteLine("The object-type value o = {0}", o);
+            Console.WriteLine("The value-type value j = {0}", j);
+
+            object p = 0;
+            o = 100;
+            Console.WriteLine("The object-type value o = {0}", o);
+            Console.WriteLine("The object -tye value p = {0}", p);
+        }
+    }
+}
