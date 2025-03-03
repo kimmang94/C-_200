25ff3af2f81559a8d741e45e23ac9a3fa9bf4519
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-03 21:44:30 +0900

    다른 변수 출력

---
+++ CSharp_200/DifferentValiablePrint/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DifferentValiablePrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 100;
+            double v2 = 1.234;
+
+            Console.WriteLine(v1.ToString() + ", " + v2.ToString());
+            Console.WriteLine("v1 = " + v1 + ", v2 = " + v2);
+            Console.WriteLine("v1 = {0}, v2 = {1}", v1, v2);
+            Console.WriteLine($"v1 = {v1}, v2 = {v2}");
+        }
+    }
+}
