d0482af72eea5a669d44a74374e26a22caf65688
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-03 22:22:10 +0900

    타입 변환

---
+++ CSharp_200/TypeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2137483647;
+            long bigNum = num;
+            Console.WriteLine(bigNum);
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x;
+            Console.WriteLine(a);
+        }
+    }
+}
