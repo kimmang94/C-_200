f0e60cd465e5a595d6e8cc331085f46fa732eed4
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-06 00:21:34 +0900

    81번 까지

---
+++ CSharp_200/AreaOfCircle/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (double r = 1; r <= 10; r++)
+            {
+                Console.WriteLine("Area of cicle with radius {0,2} = {1,7:F2}", r, AreaOfCicle(r));
+            }
+        }
+
+        private static double AreaOfCicle(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
