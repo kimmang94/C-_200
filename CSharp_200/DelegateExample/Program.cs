08ba9e2e974bef41598946afdd308e91a0b1bc1f
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-12 01:58:47 +0900

    Delegate

---
+++ CSharp_200/DelegateExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DelegateExample
+{
+    internal class Program
+    {
+        delegate bool MemberTest(int a);
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 5, 8, 54, 23, 4, 6, 4 };
+
+            Console.WriteLine("짝수의 갯수 : " + Count(arr,IsEven));
+            Console.WriteLine("홀수의 갯수 : " + Count(arr,IsOdd));
+        }
+
+        static int Count(int[] a, MemberTest testMethod)
+        {
+            int cnt = 0;
+            foreach (var n in a)
+            {
+                if (testMethod(n) ==  true)
+                {
+                    cnt++;
+                }
+                
+            }
+            return cnt;
+        }
+
+        static public bool IsOdd(int n) { return n % 2 != 0; }
+        static public bool IsEven(int n) { return n % 2 == 0; }
+    }
+}
