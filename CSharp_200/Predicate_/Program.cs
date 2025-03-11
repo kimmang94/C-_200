0ac64c7c5208cf6f30974205ae39bd6da018e946
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-12 02:38:44 +0900

    119 번 까지

---
+++ CSharp_200/Predicate_/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Predicate_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = n => n % 2 == 0;
+            Console.WriteLine(isEven(6));
+
+            Predicate<string> isLowerCase = s => s.Equals(s.ToLower());
+            Console.WriteLine(isLowerCase("This is test"));
+        }
+    }
+}
