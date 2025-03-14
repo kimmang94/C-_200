e29910cf5754b39bdad2bbb98af70b01753f486f
KIMHYEOK\borad <kimmang940715@naver.com>
2025-03-14 22:21:54 +0900

    142번 까지

---
+++ CSharp_200/CheckedListBox/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CheckedListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cListBox = new System.Windows.Forms.CheckedListBox();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnDelAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cListBox
+            // 
+            this.cListBox.FormattingEnabled = true;
+            this.cListBox.Location = new System.Drawing.Point(12, 12);
+            this.cListBox.Name = "cListBox";
+            this.cListBox.Size = new System.Drawing.Size(241, 409);
+            this.cListBox.TabIndex = 0;
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(382, 12);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(241, 407);
+            this.lstBox.TabIndex = 1;
+            // 
+            // btnTo
+            // 
+            this.btnTo.Location = new System.Drawing.Point(283, 119);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(75, 23);
+            this.btnTo.TabIndex = 2;
+            this.btnTo.Text = ">";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(283, 148);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(75, 23);
+            this.btnAll.TabIndex = 3;
+            this.btnAll.Text = ">>";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(283, 177);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnDelAll
+            // 
+            this.btnDelAll.Location = new System.Drawing.Point(283, 206);
+            this.btnDelAll.Name = "btnDelAll";
+            this.btnDelAll.Size = new System.Drawing.Size(75, 23);
+            this.btnDelAll.TabIndex = 5;
+            this.btnDelAll.Text = "모두삭제";
+            this.btnDelAll.UseVisualStyleBackColor = true;
+            this.btnDelAll.Click += new System.EventHandler(this.btnDelAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 675);
+            this.Controls.Add(this.btnDelAll);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.cListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox cListBox;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnDelAll;
+    }
+}
+
---
+++ CSharp_200/CheckedListBox/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckedListBox
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cListBox.Items.Add("오스트리아, 빈");
+            cListBox.Items.Add("호주, 애틀레이드");
+
+            lstBox.SelectionMode = SelectionMode.MultiExtended;
+        }
+
+        private void btnTo_Click(object sender, EventArgs e)
+        {
+            foreach(var city in cListBox.CheckedItems)
+            {
+                lstBox.Items.Add(city);
+            }
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            foreach (var city in cListBox.Items)
+            {
+                lstBox.Items.Add(city);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            List<string> listRemove = new List<string>();
+
+            foreach (string city in lstBox.SelectedItems)
+            {
+                listRemove.Add(city);
+            }
+
+            foreach (string city in listRemove)
+            {
+                lstBox.Items.Remove(city);
+            }
+        }
+
+        private void btnDelAll_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Clear();
+        }
+    }
+}
